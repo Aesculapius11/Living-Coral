6f557eb3f9267d0734f9e33972c48b55bc35c555
Aesculapius11 <aesculapius11@proton.me>
2026-03-13 14:56:22 +0800

    再修语言高亮

--- .eleventy.js
+++ .eleventy.js
@@ -11,8 +11,8 @@ module.exports = function (eleventyConfig) {
         Prism.languages.shell = Prism.languages.bash || Prism.languages.shell;
         Prism.languages.sh = Prism.languages.bash || Prism.languages.sh;
         Prism.languages.conf = Prism.languages.ini || Prism.languages.conf;
-        Prism.languages.docker = Prism.languages.bash || Prism.languages.docker;
-        Prism.languages.dockerfile = Prism.languages.bash || Prism.languages.dockerfile;
+        Prism.languages.docker = Prism.languages.shell || Prism.languages.docker;
+        Prism.languages.dockerfile = Prism.languages.docker || Prism.languages.dockerfile;
       } catch (e) {}
     },
   });
