aca021dd2784c214e55b6be17f70489e45a3957d
Aesculapius11 <aesculapius11@proton.me>
2026-03-13 15:11:59 +0800

    测试高亮

--- .eleventy.js
+++ .eleventy.js
@@ -11,8 +11,8 @@ module.exports = function (eleventyConfig) {
         Prism.languages.shell = Prism.languages.bash;
         Prism.languages.sh = Prism.languages.bash;
         Prism.languages.conf = Prism.languages.ini || Prism.languages.conf;
-        Prism.languages.docker = Prism.languages.bash || Prism.languages.docker;
-        Prism.languages.dockerfile = Prism.languages.bash || Prism.languages.dockerfile;
+        Prism.languages.docker = Prism.languages.bash;
+        Prism.languages.dockerfile = Prism.languages.bash;
       } catch (e) {}
     },
   });
